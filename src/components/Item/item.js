7396c5986295d09b62b7e889ec021af4f0e9dd2e
Axel Duran <duranaxel78@gmail.com>
2023-03-21 19:49:41 -0300

    Proyecto actualizado

--- src/components/Item/item.js
+++ src/components/Item/item.js
@@ -1,15 +1,12 @@
+import './item.css'
 import { Link } from "react-router-dom"
-/*import portada1 from '../../assets/peli-1.jpg'
-import portada2 from '../../assets/peli-2.jpg'
-import portada3 from '../../assets/peli-3.jpg'
-import portada4 from '../../assets/peli-4.jpg'*/
 
 const Item = ( {item} ) => {
 
     return (
-        <div className='imagen'>
+        <div className='portada'>
             <h2>{item.name}</h2>
-            <img src={item.img}/>
+            <img className='imagen' src={item.img}/>
             <p>{item.description}</p>
             <p>Precio: ${item.price}</p>
             <p><small>Categoría: {item.category}</small></p>
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,23 +1,24 @@
 import './navbar.css'
-//import logo from '../../assets/logoMarvel.png'
 import CartWidget from '../CartWidget/cartWidget'
 import { Link } from 'react-router-dom'
 
 export const Navbar = () =>  {
 
-    /*return (
+    return (
         <header className="header">
             <div className="header_container">
-                <img src={logo} className="header_logo" alt='logo'/>
+            <img src={process.env.PUBLIC_URL + '/assets/logoMarvel.png'} className="header_logo" alt='logo'/>
 
                 <nav className="navbar">
                     <Link to='/' className="navbar_link">Inicio</Link>
                     <Link to='/nosotros' className="navbar_link">Nosotros</Link>
                     <Link to='/contactanos' className="navbar_link">Contactános</Link>
+                    <Link to='/productos/Primeras Películas' className="navbar_link">Primeras Películas</Link>
+                    <Link to='/productos/Últimas Películas' className="navbar_link">Últimas Películas</Link>
                 </nav>
 
                 <CartWidget/>
             </div>
         </header>
-    )*/
+    )
 }
